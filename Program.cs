284a9fb9a17311b55b2055c2b0f4922a8a4391a3
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2021-09-10 11:26:23 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValutaOmregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
